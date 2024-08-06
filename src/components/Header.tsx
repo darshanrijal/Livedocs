320f75d394e0f45f585200ef9ff3fa88c5ebe05a
Darshan rijal <darshanrijal0@gmail.com>
2024-08-06 21:33:35 +0545

    Authenticate user with real time features

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-export const Header = ({ children }: HeaderProps) => {
+export const Header = ({ children, className }: HeaderProps) => {
   return (
-    <div className="header">
+    <div className={cn("header", className)}>
       <Link href={"/"} className="md:flex-1">
         <Image
           src={"/assets/icons/logo.svg"}
