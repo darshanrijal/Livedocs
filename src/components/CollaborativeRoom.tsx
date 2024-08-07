05f823d5c146868a7eb80fc5375270495de22623
Darshan rijal <darshanrijal0@gmail.com>
2024-08-07 18:27:54 +0545

    Completed share modal

--- src/components/CollaborativeRoom.tsx
+++ src/components/CollaborativeRoom.tsx
@@ -10,6 +10,7 @@ import { Input } from "./ui/input";
 import Image from "next/image";
 import { updateDocument } from "@/lib/actions/room.actions";
 import { Loader } from "./Loader";
+import { ShareModal } from "./ShareModal";
 
 export const CollaborativeRoom = ({
   roomId,
@@ -115,7 +116,12 @@ export const CollaborativeRoom = ({
             </div>
             <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
               <ActiveCollaborators />
-
+              <ShareModal
+                roomId={roomId}
+                collaborators={users}
+                creatorId={roomMetadata.creatorId}
+                currentUserType={currentUserType}
+              />
               <SignedOut>
                 <SignInButton />
               </SignedOut>
